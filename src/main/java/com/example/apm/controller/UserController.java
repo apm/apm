d0c8459e12a32ebe5f021079b760e6b07a8f27a9
anan3351 <102586407+anan3351@users.noreply.github.com>
2023-08-02 16:09:51 +0900

    Add files via upload

---
+++ src/main/java/com/example/apm/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.example.apm.controller;
+
+import com.example.apm.entity.SiteUser;
+import com.example.apm.form.UserCreateForm;
+import com.example.apm.repository.UserRepository;
+import com.example.apm.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String showSignupForm(UserCreateForm userCreateForm) {
+        return "signup_form"; // 뷰 템플릿으로 이동
+    }
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        // Check if userName already exists in the database
+        Optional<SiteUser> existingUser = userRepository.findByusername(userCreateForm.getUsername());
+        if (existingUser.isPresent()) {
+            bindingResult.rejectValue("username", "usernameExists",
+                    "이미 사용 중인 사용자 이름입니다.");
+            return "signup_form";
+        }
+
+        userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1());
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+}

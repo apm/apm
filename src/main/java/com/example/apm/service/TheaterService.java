8cb74e0901241502cdce36aa093ac4c961c82ccd
anan3351 <rinensi76@naver.com>
2023-08-08 03:26:59 +0900

    서울 지역 극장 조회 추가. 한글,영어,숫자 오름차순 정렬 적용 /theater/list/seoul 로 url 연결

--- src/main/java/com/example/apm/service/TheaterService.java
+++ src/main/java/com/example/apm/service/TheaterService.java
@@ -1,30 +1,56 @@
 package com.example.apm.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.stereotype.Service;
-
-import com.example.apm.DataNotFoundException;
+import com.example.apm.OrderingByKoreanEnglishNumberSpecial;
 import com.example.apm.entity.Theater;
 import com.example.apm.repository.TheaterRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class TheaterService {
 
     private final TheaterRepository theaterRepository;
 
-    public TheaterService(TheaterRepository theaterRepository) {
-        this.theaterRepository = theaterRepository;
-    }
-
     public Theater getTheater(Integer theaterId) {
         return theaterRepository.findById(theaterId).orElse(null);
     }
 
-    public List<Theater> getTheaterList() {
-        return theaterRepository.findAll();
+    public Page<Theater> getTheaterList(Pageable pageable) {
+        List<Theater> allTheaters = theaterRepository.findAll();
+        List<Theater> sortedTheaters = allTheaters.stream()
+                .sorted((t1, t2) -> OrderingByKoreanEnglishNumberSpecial.compare(
+                        t1.getTheaterName(), t2.getTheaterName()))
+                .collect(Collectors.toList());
+
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<Theater> pageTheaters;
+
+        if (sortedTheaters.size() < startItem) {
+            pageTheaters = List.of();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, sortedTheaters.size());
+            pageTheaters = sortedTheaters.subList(startItem, toIndex);
+        }
+
+        return new PageImpl<>(pageTheaters, pageable, sortedTheaters.size());
+    }
+
+    public Page<Theater> getSeoulTheaterList(Pageable pageable) {
+        Page<Theater> allTheaters = getTheaterList(pageable);
+        List<Theater> seoulTheaters = allTheaters.getContent()
+                .stream()
+                .filter(theater -> theater.getTheaterLocation().startsWith("서울"))
+                .collect(Collectors.toList());
+        return new PageImpl<>(seoulTheaters, pageable, seoulTheaters.size());
     }
 }

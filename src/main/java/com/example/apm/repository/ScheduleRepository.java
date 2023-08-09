2655c4536be97ba60cba70d1ef95a77243e7cc98
anan3351 <102586407+anan3351@users.noreply.github.com>
2023-08-09 18:06:45 +0900

    Delete src directory

--- src/main/java/com/example/apm/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.apm.repository;
-
-import com.example.apm.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
-}
